91cbb2b783b0b009e389863c14296a824b94b4d1
nkone <phtruong@protonmail.com>
2019-03-05 15:21:27 -0800

    add working files, fixed leaks, header, no multi file descriptor support, however

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 19:21:36 by phtruong          #+#    #+#             */
-/*   Updated: 2019/03/04 10:55:08 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/03/05 15:13:55 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@
 ** GENERAL INFO
 ** read function returns a non-negative integer indicating no. bytes read
 ** otherwise -1, indicating error.
-** Find a way to null-termintate garbages.
+** Find a way to null-termintate garbages.✓
+** Maybe forget bonus✓
+** Check for FD_LIMIT
+** Make test for two/ may be three files
 */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "get_next_line.h"
+#include "./libft/libft.h"
 
-/*int	token_me(char **tmp, char **line)
+int	token_me(char **tmp, char **line)
 {
 	char *tmp_h;
 	char *token;
@@ -36,10 +38,8 @@
 	token = ft_strtok_r(*tmp, "\n", &*tmp);
 	if (token != NULL)
 	{
-		*line = (char *)malloc(ft_strlen(token) * sizeof(char *));
-		ft_strcpy(*line, token);
+		*line = ft_strdup(token);
 		tmp_h = ft_strdup(*tmp);
-		//printf("tmp_h: |%s|\n", tmp_h);
 		if (tmp_h)
 		{
 			*tmp = ft_strdup(tmp_h);
@@ -50,36 +50,11 @@
 	else
 	{
 		*line = ft_strdup(*tmp);
-		ft_memdel((void **)*tmp);
+		ft_strclr(*tmp);
 	}
 	return (1);
-}*/
+}
 
-int		token_me(char **holder, char **line)
-{
-	char *tmp;
-
-	if ((*holder)[0] == '\0')
-		return (0);
-	if (ft_strchr(*holder, '\n') != NULL)
-	{
-		*(ft_strchr(*holder, '\n')) = '\0';
-		*line = ft_strdup(*holder);
-		tmp = ft_strdup(ft_strchr(*holder, '\0') + 1);
-		free(*holder);
-		if (tmp)
-		{
-			*holder = ft_strdup(tmp);
-			free(tmp);
-		}
-	}
-	else
-	{
-		*line = ft_strdup(*holder);
-		ft_memdel((void **)holder);
-	}
-	return (1);
-}/*
 int	get_next_line(const int fd, char **line)
 {
 	char		buffer[BUFF_SIZE + 1];
@@ -87,48 +62,22 @@ int	get_next_line(const int fd, char **line)
 	static char	*tmp[FD_LIMIT];
 	char		*tmp_h;
 
-	if (fd < 0 || !line || read(fd, buffer, 0) < 0)
+	if (fd == -1 || fd > FD_LIMIT || read(fd, buffer, 0) == -1)
 		return (-1);
-	if (!tmp[fd])
+	if (!(tmp[fd]))
 		tmp[fd] = ft_strnew(0);
-	while (!(ft_strchr(tmp[fd], '\n')))
+	while (ft_strchr(tmp[fd], '\n') == NULL)
 	{
 		n_bytes = read(fd, buffer, BUFF_SIZE);
+		buffer[n_bytes] = '\0';
 		if (n_bytes == 0)
-			break;
-		buffer[BUFF_SIZE] = '\0';
+			break ;
 		tmp_h = ft_strjoin(tmp[fd], buffer);
 		free(tmp[fd]);
 		tmp[fd] = ft_strdup(tmp_h);
-		if (!tmp[fd])
+		if (!(tmp[fd]))
 			tmp[fd] = ft_strnew(0);
 		free(tmp_h);
 	}
 	return (token_me(&tmp[fd], line));
-}*/
-int		get_next_line(const int fd, char **line)
-{
-	int			br;
-	char		buffer[BUFF_SIZE + 1];
-	static char	*arr[FD_LIMIT];
-	char		*tmp;
-
-	if (fd == -1 || read(fd, buffer, 0) < 0 || !line)
-		return (-1);
-	if (!(arr[fd]))
-		arr[fd] = ft_strnew(0);
-	while (ft_strchr(arr[fd], '\n') == NULL)
-	{
-		br = read(fd, buffer, BUFF_SIZE);
-		buffer[br] = '\0';
-		if (br == 0)
-			break;
-		tmp = ft_strjoin(arr[fd], buffer);
-		free(arr[fd]);
-		arr[fd] = ft_strdup(tmp);
-		if (!(arr[fd]))
-			arr[fd] = ft_strnew(0);
-		free(tmp);
-	}
-	return (token_me(&arr[fd], line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 10:05:30 by phtruong          #+#    #+#             */
-/*   Updated: 2019/03/04 09:32:09 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/03/05 15:07:23 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 # define GET_NEXT_LINE_H
 
 # include "./libft/libft.h"
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
 # define BUFF_SIZE 10
 # define FD_LIMIT 1024
 
