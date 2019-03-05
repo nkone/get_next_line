3f100f8d06b013d6167135f93689ef88c7bb90eb
nkone <phtruong@protonmail.com>
2019-03-05 15:23:16 -0800

    update library Makefile, removed header in source, updated libft.h headers with fcntl

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 14:40:25 by phtruong          #+#    #+#             */
-/*   Updated: 2019/02/24 13:29:53 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/03/05 15:08:04 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
+# include <fcntl.h>
 
 # include "ft_string.h"
 # include "ft_ctype.h"
